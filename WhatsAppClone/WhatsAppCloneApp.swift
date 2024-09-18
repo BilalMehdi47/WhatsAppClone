6fdb105b47f0cf8db3c639615e0cb9005d7862b0
Bilal Mehdi <bilalmehditk@gmail.com>
2024-09-18 11:19:19 +0500

    Initial Commit

---
+++ WhatsAppClone/WhatsAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppCloneApp.swift
+//  WhatsAppClone
+//
+//  Created by Bilal Mehdi on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
